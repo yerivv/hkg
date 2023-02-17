5c74c5523e803745e11945267a8be51678797986
yeri kim <kimyeri0915@gmail.com>
2023-02-17 18:09:11 +0900

    first commit

---
+++ src/assets/js/ui/form.js
@@ -0,0 +1,30 @@
+console.log('form');
+
+const d = new Date();
+const year = d.getFullYear();
+const month = d.getMonth();
+const day = d.getDate();
+
+const period = {
+	init(b, c){
+		let today = new Date(year, month, day).toLocaleDateString();
+		document.querySelector('#endDate').value = today;
+		period.select('', b, c);
+	},
+	select(a, b, c){
+		let newDate;
+		switch(b){
+			case 'day':
+				newDate = new Date().setDate(day - c);
+				break;
+			case 'month':
+				newDate = new Date().setMonth(month - c);
+				break;
+			case 'year':
+				newDate = new Date().setYear(year - c);
+				break;
+		}
+		//a.checked = true;
+		document.querySelector('#startDate').value = new Date(newDate).toLocaleDateString();
+	}
+}
