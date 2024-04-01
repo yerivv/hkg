2247e86cbce5cf55f75edbb58f03cdab1c3076d3
yeri <yeri_v_v@naver.com>
2024-04-01 19:24:15 +0900

    first commit

--- src/assets/js/ui/layout.js
+++ src/assets/js/ui/layout.js
@@ -1,1 +1,10 @@
 console.log('layout');
+
+const setScreenSize = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setScreenSize();
+
+window.addEventListener('resize', () => setScreenSize());
