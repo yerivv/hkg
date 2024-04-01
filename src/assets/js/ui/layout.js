b47944aec2b5bc5eb3bcd9bce06c2b156314efdc
yerivv <yeri_v_v@naver.com>
2024-04-01 22:58:07 +0900

    A-1시안 tour 작업

--- src/assets/js/ui/layout.js
+++ src/assets/js/ui/layout.js
@@ -4,7 +4,9 @@ const setScreenSize = () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
-
 setScreenSize();
-
 window.addEventListener('resize', () => setScreenSize());
+
+const scrollHeader = () => {
+  
+}
