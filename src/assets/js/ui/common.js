25954229663facbd2987024562c84fc7877ac975
yeri <yeri_v_v@naver.com>
2024-04-02 22:56:59 +0900

    디자인 시연 A-1, A-2 작업

--- src/assets/js/ui/common.js
+++
@@ -1,8 +0,0 @@
-const itemIndex = a => {
-    let items = [...a.parentElement.children];
-    return items.indexOf(a);
-};
-
-const insertAfter = (a, b) => {
-    a.parentElement.insertBefore(b, a.nextSibling);
-};
--- src/assets/js/ui/layout.js
+++ src/assets/js/ui/layout.js
@@ -1,12 +1,28 @@
 console.log('layout');
 
-const setScreenSize = () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-setScreenSize();
-window.addEventListener('resize', () => setScreenSize());
-
 const scrollHeader = () => {
-  
+  let preScrollTop = 0;
+  window.addEventListener('scroll', () => {
+    let nextScrollTop = window.scrollY;
+    const toolbarStyle = document.querySelector('.toolbar-layout');
+    
+    if(document.querySelectorAll('.toolbar-layout').length > 0) {
+      // 스크롤 방향에 따라 툴바 숨기기/보이기 결정
+      if (nextScrollTop > preScrollTop) {
+        toolbarStyle.classList.add('hide');
+      } else {
+        toolbarStyle.classList.remove('hide');
+      }
+      
+      // 페이지 맨 상단 또는 맨 하단에 위치할 때 툴바 보이기
+      if (nextScrollTop <= 0 || nextScrollTop > (document.documentElement.scrollHeight - window.innerHeight - 100)) {
+        toolbarStyle.classList.remove('hide');
+      }
+
+      // 현재 스크롤 위치를 이전 스크롤 위치로 업데이트
+      preScrollTop = nextScrollTop;
+    }
+  });
 }
+
+scrollHeader();
